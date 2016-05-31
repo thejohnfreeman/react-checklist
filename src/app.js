f872080b4e59d59420cd0e5e0e028d33be02617c
John Freeman <jfreeman08@gmail.com>
2016-05-30 23:45:13 -0400

    React implementation of TodoMVC, sans routing

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
-require('semantic-ui-css/semantic.css')
-require('./style.css')
+require('todomvc-common/base.css')
+require('todomvc-app-css/index.css')
+require('./editable-text.css')
 
 if (process.env.NODE_ENV === 'development') {
     require('./template.html')
 }
 
-// Have to include jQuery for Semantic
-window.jQuery = require('jquery')
-require('semantic-ui-css/semantic.js')
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'content.js'
 
-ReactDOM.render(<App />,  document.getElementById('content'))
+ReactDOM.render(<App/>,  document.getElementById('content'))
 
