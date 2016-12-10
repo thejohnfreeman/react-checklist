97e75ea0aef4d32db7c38c17be425b1f8d20cc41
John Freeman <jfreeman08@gmail.com>
2016-12-10 09:06:52 -0500

    Improve react-redux example
    - Name *Presentation and *Container components
    - Use lambdas
    - Use switches

--- src/checklist-react-redux.js
+++ src/checklist-react-redux.js
@@ -4,54 +4,60 @@ import {connect, Provider} from 'react-redux'
 import {createStore} from 'redux'
 import {mutate} from './mutators.js'
 
+// Actions.
+
+// TODO: Use an object that represents the type of objects for this action?
+const TOGGLE_ACTION = {}
+
+// Reducers.
+
 const defaultState = [
   {text: 'a', completed: false},
   {text: 'b', completed: true},
   {text: 'c', completed: false},
 ]
 
-const TOGGLE_ACTION = 'TOGGLE'
-
 function reducer(state = defaultState, action) {
-  if (action.type === TOGGLE_ACTION) {
-    return mutate(
-      state, [action.index, 'completed'],
-      (completed) => !completed)
+  switch (action.type) {
+    case TOGGLE_ACTION:
+      // TODO: Consider immutable.js.
+      return mutate(
+        state, [action.index, 'completed'],
+        (completed) => !completed)
   }
   return state
 }
 
-const store = createStore(reducer)
+// Action creators.
 
-function toggle(index) {
-  return {
-    type: TOGGLE_ACTION,
-    index: index,
-  }
-}
+const toggle = (index) => ({
+  type: TOGGLE_ACTION,
+  index: index,
+})
 
-const TodoTemplate = ({text, completed, onCheck}) => (
+// Components.
+
+const TodoPresentation = ({text, completed, onCheck}) => (
   <li className="item" style={{backgroundColor: completed ? 'lightgreen' : 'lightcoral'}}>
     <input type="checkbox" checked={completed} onChange={onCheck} />
     {text}
   </li>
 )
-
 const mapStateToPropsTodo = (state, props) => (state[props.index])
 const mapDispatchToPropsTodo = (dispatch, props) => ({
-  onCheck: () => dispatch(toggle(props.index))
+  onCheck() { dispatch(toggle(props.index)) }
 })
-const Todo = connect(mapStateToPropsTodo, mapDispatchToPropsTodo)(TodoTemplate)
+const TodoContainer = connect(mapStateToPropsTodo, mapDispatchToPropsTodo)(TodoPresentation)
 
-const TodoListTemplate = ({todos}) => (
+const TodoListPresentation = ({todos}) => (
   <ul>
-    {todos.map((t, i) => <Todo key={i} index={i} />)}
+    {todos.map((t, i) => <TodoContainer key={i} index={i} />)}
     <li key={-1}>{todos.filter(t => !t.completed).length} items left</li>
   </ul>
 )
 const mapStateToPropsTodoList = (state) => ({todos: state})
-const TodoList = connect(mapStateToPropsTodoList)(TodoListTemplate)
+const TodoListContainer = connect(mapStateToPropsTodoList)(TodoListPresentation)
 
 ReactDOM.render(
-  <Provider store={store}><TodoList /></Provider>,
+  <Provider store={createStore(reducer)}><TodoListContainer /></Provider>,
   document.getElementById('checklist-react-redux'))
